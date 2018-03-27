6f177c913631c85e3f7cd1618fcf92eda3163185
ptapia <pabloignaciotapia@gmail.com>
2018-03-27 11:58:17 -0300

    pruebaas

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 
+declare const $: any;
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  {path: 'dashboard', title: 'Dashboard', icon: 'dashboard', class: ''},
+  {path: 'user-profile', title: 'User Profile', icon: 'person', class: ''},
+  {path: 'table-list', title: 'Table List', icon: 'content_paste', class: ''},
+  {path: 'typography', title: 'Typography', icon: 'library_books', class: ''},
+  {path: 'icons', title: 'Icons', icon: 'bubble_chart', class: ''},
+  {path: 'maps', title: 'Maps', icon: 'location_on', class: ''},
+  {path: 'notifications', title: 'Notifications', icon: 'notifications', class: ''},
+  {path: 'upgrade', title: 'Upgrade to PRO', icon: 'unarchive', class: 'active-pro'},
+];
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +26,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
 
 }
